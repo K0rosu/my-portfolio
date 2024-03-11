1e23bcc9bf450ce6baa4f1786e0789731d6fd8ed
K0rosu <craft.seto@gmail.com>
2024-03-11 11:36:49 -0300

    fix

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -11,7 +11,13 @@ const Hero = () => {
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
         display: "flex",
-        alignItems: "center"
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]: {
+            paddingTop: "100px",
+        },
+        [theme.breakpoints.up('md')]: {
+            paddingTop: "0",
+        }
     }))
 
     const StyledImg = styled("img")(({ theme }) => ({
